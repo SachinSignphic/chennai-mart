396cc2aff873e56f18b2b5f4b6d7a8f0e912b893
Sachin / <sreesachin11226@gmail.com>
2023-12-13 23:10:12 +0530

    [app] tailwind + bash commands + expo router

---
+++ app.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Change directory to the "app" folder
+cd ./app
+
+# Run the command "npm start-tunnel"
+npm run start
---
+++ server.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Change directory to the "server" folder
+cd ./server
+
+# Run the command "npm run dev"
+npm run dev
