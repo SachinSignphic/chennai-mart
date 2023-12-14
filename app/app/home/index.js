b68b7164ea0ac1c612e0c96bae586011219116f5
Sachin / <sreesachin11226@gmail.com>
2023-12-14 20:33:39 +0530

    [app] tab routing started

---
+++ app/app/home/index.js
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const home = () => {
+  return (
+    <View className="bg-slate-500">
+      <Text className="text-teal-100">Home Screen</Text>
+    </View>
+  )
+}
+
+export default home
--- app/app/index.js
+++ app/app/index.js
@@ -1,12 +1,14 @@
+import { Link } from "expo-router";
 import { StatusBar } from "expo-status-bar";
 import { Text, View } from "react-native";
 
 export default function App() {
     return (
         <View className='flex-1 justify-center items-center'>
-            <Text className='text-lg text-red-500'>
+            <Text className='text-xl text-red-500'>
                 Open up App.js to start working on your app!
             </Text>
+            <Link href={'/home'}>Go to home!</Link>
             <StatusBar style='auto' />
         </View>
     );
