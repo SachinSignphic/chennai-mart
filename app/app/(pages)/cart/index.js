40e2d04f37ecb0e225094dc3a5d8d19bae8c90a6
Sachin / <sreesachin11226@gmail.com>
2023-12-31 00:27:09 +0530

    [app] cart page bug fix

--- app/app/(pages)/cart/index.js
+++ app/app/(pages)/cart/index.js
@@ -1,13 +1,14 @@
-import { View, Text, Image } from "react-native";
+import { View, Text, Image, TouchableOpacity } from "react-native";
 import React from "react";
 import { ScrollView } from "react-native-gesture-handler";
-import { SimpleLineIcons } from "@expo/vector-icons";
+import { Ionicons } from "@expo/vector-icons";
 import { useSelector } from "react-redux";
 import { ProductCartAction } from "@components";
+import { router } from "expo-router";
 
 const CartItemCard = ({ productId, productName, productQty, quantity, price, image }) => {
   return (
-      <View className='flex flex-row mt-2 border justify-between items-center border-primary/40 rounded-xl px-3 py-2'>
+      <View className='flex flex-row mt-2 border justify-between items-center border-primary/10 rounded-xl px-3 py-2'>
           <View className='w-1/5 p-2'>
               <Image
                   resizeMode='center'
@@ -39,6 +40,7 @@ const CartItemCard = ({ productId, productName, productQty, quantity, price, ima
 const index = () => {
     const productData = useSelector((state) => state.products.products);
     const cartData = useSelector((state) => state.cart.items);
+    let totalCartAmount = cartData.map(item => productData.find(product => product.id == item.id).price * item.quantity).reduce((prev, curr) => prev + curr, 0);
 
     return (
         <>
@@ -60,8 +62,44 @@ const index = () => {
                     );
                 })}
             </ScrollView>
-            <View className='absolute bottom-0 left-0 w-full bg-teal z-50'>
-                <Text>Hi da</Text>
+            <View className='absolute shadow-xl shadow-black/40 bottom-0 left-0 w-full bg-teal gap-y-3 z-50 px-4 py-4'>
+                <TouchableOpacity
+                    className='flex flex-row justify-between'
+                    onPress={() => router.push("/cart/addresses")}>
+                    <Text className='text-secondary font-medium text-base'>
+                        No.8, 9th cross street, thirumudivakkam, Chennai
+                    </Text>
+                    <View className='flex flex-row'>
+                        <Ionicons
+                            name='location-sharp'
+                            size={20}
+                            color={"rgb(47,46,65)"}
+                        />
+                        <Text className='font-nunito-800 text-md text-primary'>
+                            Change
+                            <Ionicons
+                                name='chevron-down-sharp'
+                                size={16}
+                                color='black'
+                            />
+                        </Text>
+                    </View>
+                </TouchableOpacity>
+                <View className='flex flex-row justify-between gap-x-5 items-center'>
+                    <View>
+                        <Text className='text-lg font-nunito-400 text-primary'>
+                            To Pay
+                        </Text>
+                        <Text className='text-xl font-nunito-800 text-primary'>
+                            ₹{totalCartAmount}
+                        </Text>
+                    </View>
+                    <TouchableOpacity className='flex flex-1 py-3 bg-primary rounded-lg'>
+                        <Text className='text-white self-center font-nunito-800 text-lg'>
+                            Proceed ▶
+                        </Text>
+                    </TouchableOpacity>
+                </View>
             </View>
         </>
     );
